83c4e1f0318dcf355bcbe6e008b5c219843a99bf
Felipe Spirandelli <felipereisspirandelli@gmail.com>
2024-06-09 16:06:59 -0300

    feat: upgrade client to handle multiple thread

--- src/users.cpp
+++ src/users.cpp
@@ -5,74 +5,75 @@
 #include <ctime>
 #include <iostream>
 #include <netinet/in.h>
+#include <pthread.h>
 #include <string>
 #include <sys/socket.h>
 #include <unistd.h>
 
-Users::Users(double p_msg, double p_u, int n, int port) : p_message(p_msg), p_user(p_u), active(false) {
+int number_of_recused = 0;
+Users::Users(double p_msg, double p_u, int n, int port)
+    : p_message(p_msg), p_user(p_u), server_port(port), active(false) {
     users.reserve(n);
     for (int i = 0; i < n; i++) {
         users.push_back("user_" + std::to_string(i));
     }
+}
+
+int Users::create_connection(int server_port) {
     sockaddr_in address;
     address.sin_family = AF_INET;
-    address.sin_port = htons(port);
+    address.sin_port = htons(server_port);
     address.sin_addr.s_addr = INADDR_ANY;
-    server_address = address;
-    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (client_socket == -1) {
         perror("Couldn't create the socket");
         exit(EXIT_FAILURE);
     }
-}
-
-void Users::create_connection() {
-    int err = connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address));
-
-    if (err == -1) {
+    if (connect(client_socket, (struct sockaddr *)&address, sizeof(address)) == -1) {
         perror("Unable to estabelish a connection. Try again later.");
-        exit(EXIT_FAILURE);
+        number_of_recused++;
     }
-
-    active = true;
-    std::cout << "starting simulation...\n";
+    return client_socket;
 }
 
-void Users::close_connection() {
+void Users::handle_message(int client_socket) {
+    int src_id, dest_id;
+    char buffer[1024] = {0};
+    int err = recv(client_socket, buffer, 1024, 0);
+    if (err == -1) {
+        perror("Erro while receiving the message.");
+        return;
+    }
+    std::string message(buffer);
+    if (!message.empty()) {
+        std::string msg = decode_message(message, src_id, dest_id);
+    }
+}
+
+void Users::close_connection(int client_socket) {
     int err = close(client_socket);
 
     if (err == -1) {
         perror("Unable to close the connection.");
         exit(EXIT_FAILURE);
     }
-
-    active = false;
-    std::cout << "ending simulation...\n";
 }
 
-void Users::send_message(int src_id, int dest_id, std::string message) {
-    std::string encoded_msg = encode_message(src_id, dest_id, message);
-    int err = send(client_socket, encoded_msg.c_str(), encoded_msg.size(), 0);
-    if (err == -1) {
+void *Users::send_message(void *argc) {
+    thread_data *data = (thread_data *)argc;
+    data->set_client_socket(create_connection(data->server_port));
+    std::string encoded_msg =
+        encode_message(data->src_id, data->dest_id, "hello there " + std::to_string(1));
+    if (send(data->client_socket, encoded_msg.c_str(), encoded_msg.size(), 0) == -1) {
         perror("Could not sent the message.");
         exit(EXIT_FAILURE);
     }
-}
+    std::cout << "sent message" << std::endl;
 
-void Users::handle_message() {
-    int src_id, dest_id;
-    char buffer[1024] = {0};
-    // int err = recv(client_socket, buffer, sizeof(buffer), 0);
-    int err = recv(client_socket, buffer, 1024, 0);
-    if (err == -1) {
-        perror("Erro while receiving the message.");
-        exit(EXIT_FAILURE);
-    }
-    std::string message(buffer);
-    if (!message.empty()) {
-        std::string msg = decode_message(message, src_id, dest_id);
-        std::cout << "from: " << src_id << "; to: " << dest_id << "; msg: " << msg << "\n";
-    }
+    handle_message(data->client_socket);
+    close_connection(data->client_socket);
+    delete data;
+    pthread_exit(NULL);
 }
 
 void Users::create_user(std::string name) {
@@ -83,25 +84,28 @@ void Users::create_user(std::string name) {
 }
 
 void Users::simulate() {
-    create_connection();
-    int i = 0;
+    srand((unsigned)time(NULL));
+    pthread_t thread;
     while (true) {
-        srand((unsigned)time(NULL));
-        if ((double)std::rand() / RAND_MAX > p_message) {
+        if ((double)std::rand() / RAND_MAX < p_message) {
             int src_id = std::rand() % users.size();
             int dest_id = std::rand() % users.size();
-            send_message(src_id, dest_id, "hello there " + std::to_string(i++));
-            handle_message();
+            thread_data *data = new thread_data(src_id, dest_id, server_port);
+            if (pthread_create(&thread, NULL, send_message, (void *)data)) {
+                perror("Error creating thread!");
+                exit(EXIT_FAILURE);
+            }
         }
-        if ((double)std::rand() / RAND_MAX > p_user) {
-            std::string name = std::to_string(users.size()) + "_user";
-            users.push_back(name);
-        }
+        sleep_max_ms(500);
     }
-    close_connection();
+    pthread_join(thread, NULL);
 }
 
 Users::~Users() {
-    if (active)
-        close_connection();
+    std::cout << "recused: " << number_of_recused << std::endl;
+    if (active) {
+        close_connection(client_socket);
+        active = false;
+        std::cout << "ending simulation...\n";
+    }
 }
--- src/users.h
+++ src/users.h
@@ -6,20 +6,29 @@
 #include <sys/socket.h>
 #include <vector>
 
+struct thread_data {
+    int client_socket, dest_id, src_id, server_port;
+
+    thread_data(int dest_id, int src_id, int server_port)
+        : dest_id(dest_id), src_id(src_id), server_port(server_port) {}
+
+    void set_client_socket(int client_socket) { this->client_socket = client_socket; }
+};
+
 class Users {
     std::vector<std::string> users;
     double p_message, p_user;
     int client_socket;
-    sockaddr_in server_address;
+    int server_port;
     bool active;
-    void send_message(int src_id, int dest_id, std::string message);
-    void handle_message();
-    void create_connection();
-    void close_connection();
+    static void *send_message(void *argc);
+    static void handle_message(int client_socket);
+    static int create_connection(int server_port);
+    static void close_connection(int client_socket);
     void create_user(std::string name = "");
 
   public:
-    Users(double p_message, double p_user, int num_users=0, int port=8080);
+    Users(double p_message, double p_user, int num_users = 0, int port = 8080);
     ~Users();
     void simulate();
 };
